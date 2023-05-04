f55a91debd31064c3a88b8cba708b2fbf2d97d19
MaxBille <maxbille95@gmail.com>
2023-05-04 17:15:51 +0200

    plotting

--- myTest/plotting.py
+++ myTest/plotting.py
@@ -2,19 +2,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # data source
-data = np.loadtxt("/home/max/Desktop/plots/data/Collision_DpY19.CSV", delimiter=";")
-lines = plt.plot(data[0], data[1], data[0], data[2], data[0], data[3])
-plt.setp(lines, ls="--", lw=1, marker=".")
+data = np.genfromtxt("/home/max/Desktop/plots/data/St_compare.CSV", usemask=True, delimiter=";")
+re = data[0,:]
+mydata = data[1,:]
+#re20 = data[2:,0]
+#re40 = data[2:,1]
+re80 = data[2:,0]
+re100 = data[2:,1]
+re200 = data[2:,2]
+#re300 = data[2:,5]
+lines = plt.plot(#[20]*len(re20),re20,
+                 #[40]*len(re40),re40,
+                 [80]*len(re80),re80,
+                 [100]*len(re100),re100,
+                 [200]*len(re200),re200,
+                 #[300]*len(re300),re300
+                 )
+plt.setp(lines, ls="", lw=1, marker="+", color="tab:blue")
+lines = plt.plot(re,mydata)
+plt.setp(lines, ls="--", lw=1, marker=".", color="tab:orange")
 
-plt.xlabel("GPD")
-plt.ylabel("$C_{D}$")
-#plt.xlim([0,50])
+print(re)
+print(mydata)
+
+plt.xlabel("Re")
+plt.ylabel("$St$")
+#plt.xlim([0,165])
+plt.ylim([0,0.3])
 plt.grid()
-plt.title("Widerstandsbeiwert $C_{D}$ in Abhängigkeit der Auflösung in GPD für verschiedene Kollisionsoperatoren, für Re = 200", wrap=True)
-plt.legend(["BGK", "KBC", "Regularized"])
+#plt.xticks(np.arange(1990, 2010+5, 5.0))
+plt.legend(["Literatur"])
+plt.title("Strouhal-Zahl $St$ für verschiedene Reynoldszahlen, \nVergleich mit Literatur", wrap=True)
 
-literature = [1.4,1.31,1.19,1.31,1.33,1.172,1.29,1.45,1.26,1.36,1.4087]
-for lit in literature:
-    plt.axhline(y=lit, color="r", ls="--", lw=0.5)
-plt.savefig("/home/max/Desktop/plots/Collision_DpY19.png")
+plt.savefig("/home/max/Desktop/plots/St_compare.png")
 plt.show()
